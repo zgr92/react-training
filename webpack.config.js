c5b95db9371897dfb6f730f393e6add44b215014
zgr92 <software@thebeginning.pl>
2020-04-29 11:02:33 +0200

    dev webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,49 @@
+const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
-        },
-        {
-          test: /\.html$/,
-          use: {
-            loader: "html-loader"
-          }
+var config = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         }
-      ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-          template: "./src/index.html",
-          filename: "./index.html"
-        })
-      ]
-  };
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: "html-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin({cleanStaleWebpackAssets: false}),
+    new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+    }),
+  ],
+};
+
+module.exports = (env, argv) => {
+
+  if (argv.mode === 'development') {
+    config.devtool = 'inline-source-map';
+    config.devServer.contentBase = './dist';
+  }
+
+  if (argv.mode === 'production') {
+    
+  }
+
+  return config;
+};
