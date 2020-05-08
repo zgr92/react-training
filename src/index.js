dd6ebd30f4b9b8c0d5689f5e04e4c48a018e4507
zgr92 <software@thebeginning.pl>
2020-05-08 15:31:46 +0200

    changed to functional components

--- src/index.js
+++ src/index.js
@@ -1,146 +1,90 @@
 'use strict';
 
-import React, { Component } from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-class AddTaskInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { name: '' };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+function AddTaskInput(props) {
+  const [name, setName] = useState('');
 
-  handleChange(event) {
-    this.setState({ name: event.target.value });
-  }
+  const handleChange = (event) => setName(event.target.value);
 
-  handleSubmit(event) {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    this.props.onTaksAdd(this.state.name);
-    this.setState({ name: '' });
+    props.onTasksAdd(name);
+    setName('');
   }
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <input type="text" value={this.state.name} onChange={this.handleChange}></input>
-        <input type="submit" value="Submit" />
-      </form>
-    )
-  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={name} onChange={handleChange}></input>
+      <input type="submit" value="Submit" />
+    </form>
+  )
 }
 
-class Task extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.state = { done: props.done };
-    this.name = props.name;
-    this.id = props.id;
+function Task(props) {
+  const [done, setDone] = useState(props.done);
+  
+  const handleChange = (event) => {
+    setDone(event.target.checked);
+    props.onFinished(props.id);
   }
-
-  handleChange(event) {
-    const done = event.target.checked;
-    this.setState({ done });
-    this.props.onFinished(this.id);
-  }
-
-  render() {
-    return (
-      <div>
-        <input type="checkbox" checked={this.state.done} onChange={this.handleChange} />
-        <label>{this.name}</label>
-      </div>
-    );
-  }
+  
+  return (
+    <div>
+      <input type="checkbox" checked={done} onChange={handleChange} />
+      <label>{props.name}</label>
+    </div>
+  );
 }
 
-class TasksList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.idGenerator = 1;
-    this.state = {
-      tasks: [ 
-        {
-          id: this.getNextId(),
-          name: 'Task 1',
-          done: true
-        },
-        {
-          id: this.getNextId(),
-          name:'Task 2',
-          done: false,
-        },
-        {
-          id: this.getNextId(),
-          name: 'Task 3',
-          done: true,
-        }
-      ],
-    }
-    this.onTaksAdd = this.onTaksAdd.bind(this);
-    this.onFinished = this.onFinished.bind(this);
-  }
+let taskIdGenerator = 1;
 
-  getNextId() {
-    return this.idGenerator++;
-  }
+function TasksList(props) {
+  const getNextId = () => taskIdGenerator++;
+  const [tasks, setTasks] = useState(
+    [ 
+      {
+        id: getNextId(),
+        name: 'Task 1',
+        done: true
+      },
+      {
+        id: getNextId(),
+        name:'Task 2',
+        done: false,
+      },
+      {
+        id: getNextId(),
+        name: 'Task 3',
+        done: true,
+      }
+    ]
+  );
 
-  onTaksAdd(name) {
-    const tasks = this.state.tasks;
+
+  const onTasksAdd = (name) => {
     const newTask = {
-      id: this.getNextId(),
+      id: getNextId(),
       done: false,
       name,
     };
-    tasks.push(newTask);
-    this.setState({tasks});
+    setTasks(tasks => [...tasks, newTask]);
   }
 
-  onFinished(id) {
-    const tasks = this.state.tasks.map((task) => {
-      return task.id === id ? { ...task, done: true } : task;
-    });
+  const onFinished = (id) => setTasks(tasks.map((task) => task.id === id ? { ...task, done: true } : task));
 
-    this.setState({tasks})
-  }
-  
-  render() {
-    const tasks = this.state.tasks;
-
-    const isDone = (task) => {
-      return task.done;
-    }
-
-    const isUndone = (task) => {
-      return !task.done;
-    }
-
-    const tasksList = tasks.filter(isUndone).map((task) => {
-      return (
-        <Task name={task.name} done={task.done} id={task.id} onFinished={this.onFinished} key={task.id} />
-      );
-    });
-
-    const doneTasks = tasks.filter(isDone).map((task) => {
-      return (
-        <li key={task.id}>{task.name}</li>
-      );
-    });
-
-    return (
-      <div>
-          <h1>Lista zadań do zrobienia:</h1>
-          <AddTaskInput onTaksAdd={this.onTaksAdd} />
-          {tasksList}
-          <h2>Skończone zadania:</h2>
-          <ul>
-            {doneTasks}
-          </ul>
-      </div>
-      );
-  }
+  return (
+    <div>
+        <h1>Lista zadań do zrobienia:</h1>
+        <AddTaskInput onTasksAdd={onTasksAdd} />
+        {tasks.filter(task => !task.done).map(task => <Task name={task.name} done={task.done} id={task.id} onFinished={onFinished} key={task.id} />)}
+        <h2>Skończone zadania:</h2>
+        <ul>
+          {tasks.filter(task => task.done).map(task => <li key={task.id}>{task.name}</li>)}
+        </ul>
+    </div>
+    );
 }
 
 const domContainer = document.querySelector('#container');
